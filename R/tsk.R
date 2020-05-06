4dcf860eb7e3f145dc3006badcc9c09084e1fdf8
bczernecki <nwp@amu.edu.pl>
2020-05-06 17:35:50 +0200

    tsk

--- R/tsk.R
+++ R/tsk.R
@@ -12,7 +12,7 @@ library(parallel)
 args = commandArgs(trailingOnly=TRUE)
 print(args[1])
 print(args[2])
-#args <- c(20200430,1)
+#args <- c(20200502,1)
 start_wrf <- as.Date(paste0(args[1]), format="%Y%m%d")
 dzien <- as.numeric(as.character(args[2])) # tutaj chodzi o dzien wyprzedzenia wzgledem startu prognozy (np. +1, +2)
 patt <- as.character(start_wrf+dzien)
@@ -25,17 +25,11 @@ if(length(day[grep(pattern = "d03", x = day)])>0) day <- day[grep(pattern = "d03
 day <- as.list(day) 
 
 # warstwy GIS:
-load(file = "data/gisy.Rdata")
-# wojewodztwa <- readOGR("data/POL_adm1.shp")
-# pol <- readOGR("data/POL_adm0.shp")
-# rzeki <- readOGR("data/rzekiPL.shp") 
-# jeziora <- readOGR("data/jeziora.shp")
-# proj4 <- "+proj=lcc +lat_1=49.826000213623 +lat_2=49.826000213623 +lat_0=51.8421516418457 +lon_0=16.2469997406006 +x_0=0 +y_0=0 +a=6370000 +b=6370000 +units=m +no_defs"
-# wojewodztwa <- spTransform(wojewodztwa,proj4)
-# pol <- spTransform(pol, proj4)
-# jeziora <- spTransform(jeziora, proj4)
-# rzeki <- spTransform(rzeki, proj4)
-# centroidy <-  gCentroid(wojewodztwa,byid=TRUE)
+load(file = "/home/wh/github/drought/data/gisy2.Rdata")
+library(raster)
+r_baza = raster("~/tavg/tavg_2019-10-01.tif")
+crs <- crs(r_baza)
+
 
 
 myfunction_for_converting <- function(input = 'netcdf',  variable = "var"){
@@ -43,26 +37,74 @@ myfunction_for_converting <- function(input = 'netcdf',  variable = "var"){
   proj4<- GetProj(geofile)
   output <- paste0(input, "_", variable, "_.tif")
   ExportGeogrid(inFile = input, inVar = variable, outFile = output)
+  r = raster(output)
+  r = projectRaster(from = r, to = r_baza)
+  writeRaster(r, filename = output, overwrite=T)
 }
 
 mclapply(day, function(x) myfunction_for_converting(input = x, variable="TSK"), mc.cores = 24)
 # powinnismy miec teraz stworzone geotify
 
+# od razu je przekopiujmy do home'a:
+tski = dir(path=pathway, pattern = "TSK_.tif",full.names = T)
+oryg = as.POSIXct(basename(tski), format = "wrfout_d03_%Y-%m-%d_%H:%M:%S_TSK_.tif")
+nowe = paste0("/home/wh/tsk/godzinowe/", as.character(format(oryg, "%Y-%m-%d_%H_%M_%S_tsk.tif")))
+file.copy(tski, nowe, overwrite = T)
 
 ##########################################################################
 # liczymy srednia z poszczegolnych warstw:
 tempfil<-dir(path=pathway, pattern = "TSK", full.names = T)
-if(length(tempfil[grep(pattern = patt, x = tempfil)])>24) tempfil <- tempfil[grep(pattern = paste0("d03_",patt), x = tempfil)] # i tylko domena 03
+if(length(tempfil[grep(pattern = patt, x = tempfil)])>24) {
+  tempfil <- tempfil[grep(pattern = paste0(patt), x = tempfil)] # tylko dany dzien
+}
 tempfil<- stack(tempfil[grepl(pattern=patt, tempfil)])
 mintemp<- calc(tempfil,min)
+# zapis rastra
+writeRaster(mintemp, filename = paste0("/home/wh/tsk/tmin_dobowe/", 
+                                       patt, "_tsk_tmin.tif"),
+            overwrite=TRUE)
+
+##########################################################################
+# teraz czas na policzenie liczby godzin w dobie
+rc = reclassify(tempfil, c(-Inf,273.15,1, 273.15,+Inf,0))
+liczba_godzin = calc(x = rc, sum)
+# TODO:
+# zapis jako geotif
+writeRaster(liczba_godzin, filename = paste0("/home/wh/tsk/czas_przymrozku_dobowo/", 
+                                             patt, "_tsk_czas_przymrozku.tif"),
+            overwrite=TRUE)
 
 
-#beginCluster(4)
-#meantemp <- clusterR(tempfil, calc, args=list(mean, na.rm=T))
-#endCluster()
+##########################################################################
+## i na koniec zliczenie skumulowanej liczby dni na plusie
+###############################################
+## plan jest taki, zeby liczyc to na podstawie dobowek
 
-# Creating figure
-#color scale
+setwd("/home/wh/tsk/tmin_dobowe/")
+rok = format(Sys.Date()-1, "%Y")
+files = dir(pattern = as.character(rok))
+r = stack(files)
+rc = reclassify(r, c(-Inf,273.15,1, 273.15,+Inf,0))
+doy = rc # empty raster for doy
+for (i in 1:length(names(doy)))    doy[[i]][] = i
+doy = rc * doy
+  
+# ostatni mroz mozna policzyc jako:
+maksy = calc(doy, max)
+    # usuniecie zer glownie dla baltyku:
+maksy[maksy==0] = NA
+    # odejmujemy nr dzisiejszego dnia:
+ostatni_mroz = i-maksy
+#plot(ostatni_mroz)
+writeRaster(ostatni_mroz, filename = paste0("/home/wh/tsk/ostatni_mroz/", 
+                                             patt, "_tsk_ostatni_mroz.tif"),
+            overwrite=TRUE)
+
+
+######### ROZPOCZECIE RYSOWANEK: ##########
+
+# Creating figure 4 temp
+# color scale
 
 tempcolores<- c("#f6c39f","#e3ac89","#cb9881","#b58575","#9c716e","#865c62","#704754",
                 "#57344a","#3f1f3f","#240d2b","#260225","#2e0331","#370938","#420a40",
@@ -85,11 +127,8 @@ tempcolores<- c("#f6c39f","#e3ac89","#cb9881","#b58575","#9c716e","#865c62","#70
 temperatura_map<- function(input="inp", output="outp", title = "tytul"){
   
   obj1<- mask(input-273.15, pol)
-  
   centroidy$column <- sprintf(round(as.vector(raster::extract(obj1, centroidy)),1),fmt = '%#.1f')
-  
   breaks <-round(seq(-28, 35, length.out = length(tempcolores)),1)
-  
   range_min <- floor(quantile(obj1, p=0.01))
   range_max <- ceiling(max(maxValue(obj1)))
   
@@ -165,9 +204,7 @@ temperatura_map<- function(input="inp", output="outp", title = "tytul"){
     # windhydro credits
     tm_credits("esusza.pl\n(c) WIND-HYDRO 2020", position = c("left", "bottom"), 
                size = 0.35, bg.color = "white")
-  
 }
-
 
 www_path <- gsub(x = pathway, pattern = "wrfprd","www")
 dir.create(www_path)
@@ -177,10 +214,10 @@ p <- temperatura_map(input=mintemp, title = paste0("Minimalna dobowa temperatura
 # ciecie w inner margins: dol, lewa, gora, prawa
 tmap_save(p + tm_layout(inner.margins = c(-0.1, -0.06, -0.1, -0.1)), 
           filename = paste0(www_path, "/tsk_min_",patt,".png"), width=1000, height=1300)
+tmap_save(p + tm_layout(inner.margins = c(-0.1, -0.06, -0.1, -0.1)), 
+          filename = paste0("/home/wh/tsk/tmin_dobowe_png/", patt, "_tsk_tmin.png"), width=1000, height=1300)
 
-writeRaster(meantemp-273.15, filename = paste0(www_path,"/tsk_min_",patt,".tif"),overwrite=TRUE)
-writeRaster(meantemp-273.15, filename = paste0("/home/wh/tsk/tsk_min_",patt,".tif"),overwrite=TRUE)
-
+#writeRaster(mintemp-273.15, filename = paste0(www_path,"/tsk_tmin_",patt,".tif"),overwrite=TRUE)
 
 # dorzucenie kodu do plotowania rastrow godzinowych:
 nazwy <- gsub( x = gsub(x = gsub(x = names(tempfil), pattern = "wrfout_d03_", ""), pattern = "00_TSK_", ""), pattern = "_", " ")
@@ -191,6 +228,200 @@ for (i in 1:length(names(tempfil))){
   
   tmap_save(p + tm_layout(inner.margins = c(-0.1, -0.06, -0.1, -0.1)), 
             filename = paste0(www_path, "/tsk_",fname[i],".png"), width=1000, height=1300)
-  
 }
 
+#######
+##### Rysowanki dla dnia z ostatnim mrozem
+
+library(fields)
+SKALA=c('purple4','lightblue4','mediumseagreen','yellow2','orange','darkorange2','red','darkred','maroon','hotpink')
+rbPal <- colorRampPalette(SKALA)
+tempcolores = rbPal(91)
+
+temperatura_map<- function(input="inp", output="outp", title = "tytul"){
+  
+  obj1<- mask(input, pol)
+  centroidy$column <- round(as.vector(raster::extract(obj1, centroidy)))
+  breaks2 <-round(seq(0, 90, length.out = length(tempcolores)),1)
+  
+  tempcolores2 <- tempcolores
+  
+  tm_shape(obj1) +
+    tm_raster(title= title,
+              interval.closure = "left",legend.hist = T,
+              palette = tempcolores2, breaks=breaks2, 
+              legend.is.portrait = FALSE,
+              interpolate = FALSE)  +
+    
+    #Border  
+    tm_shape(pol) +
+    tm_polygons(alpha = 0.001, lwd=1.5) +
+    
+    #Border of counties 
+    tm_shape(wojewodztwa)+
+    tm_polygons(alpha = 0.01, lwd=0.7)+
+    
+    #Rivers    
+    tm_shape(rzeki)+
+    tm_lines(col="#2669d6", lwd=1.1) +
+    
+    #Lakes
+    tm_shape(jeziora)+
+    tm_polygons(col="#2669d6") +
+    
+    
+    #Title of the figure
+    tm_layout(
+      aes.palette = "div",
+      sepia.intensity = 0.2,
+      legend.just = "right",
+      title.color = "blue",
+      compass.type = "arrow",
+      title.bg.color = "white", 
+      title.bg.alpha = 0.5,
+      title.size = 45,
+      #title.position = c(0.02,0.06),
+      legend.outside = T,
+      legend.outside.position =  "bottom",
+      legend.width = 5,
+      legend.hist.width = 0.9,
+      legend.hist.height = 0.6,
+      legend.title.size = 0.90,
+      legend.text.size = 0.5,
+      #legend.position = c("right","bottom"),
+      legend.bg.color = "#FFFFFF60",
+      legend.height = 0.9,
+      legend.frame.lwd = 0.2,
+      legend.frame = F,
+      legend.bg.alpha = 1,
+      space.color="grey90"
+      #legend.format = list(text.separator = " ", format = formatC("f"))
+    )+
+    
+    #Lon/Lat    
+    tm_grid(projection = "longlat", x = 10:30, y=40:60, labels.col = "black", col = "gray",lwd = 0.5,
+            labels.size = 0.4, labels.inside.frame = T) + 
+    #Mean values of counties
+    tm_shape(centroidy)+
+    tm_text("column", size = 0.6) +   
+    
+    #Compass
+    tm_compass(size = 1, fontsize = 0.7,
+               position = c(0.04,0.9), color.light = "grey90") +
+    
+    # scale bar
+    tm_scale_bar(width = 0.12,size = 0.35,breaks = c(0,50,100,150), position = c("left","bottom")) +
+    
+    # windhydro credits
+    tm_credits("esusza.pl\n(c) WIND-HYDRO 2020", position = c("left", "bottom"), 
+               size = 0.35, bg.color = "white")
+}
+
+  p = temperatura_map(input=ostatni_mroz, title = paste0("Liczba dni od ostatniego przymrozku\n", patt))
+  tmap_save(p,
+    filename = paste0("/home/wh/tsk/ostatni_mroz_png/", 
+                      patt, 
+                      "_tsk_ostatni_mroz.png"), 
+    width=1000, height=1300)
+
+  
+  
+###############################
+### i na koniec rysowanki dla czasu przymrozku:
+
+SKALA=c('white', "lightblue", 'blue', "darkblue", "violet")
+rbPal <- colorRampPalette(SKALA)
+tempcolores = rbPal(24)
+
+  
+  temperatura_map = function(input="inp", output="outp", title = "tytul"){
+    
+    obj1<- mask(input, pol)
+    
+    centroidy$column <- round(as.vector(raster::extract(obj1, centroidy)))
+    
+    breaks2 <-round(seq(0, 24, length.out = length(tempcolores)),1)
+    tempcolores2 <- tempcolores
+    
+    tm_shape(obj1) +
+      tm_raster(title= title,
+                interval.closure = "left",legend.hist = T,
+                palette = tempcolores2, breaks=breaks2, 
+                legend.is.portrait = FALSE,
+                interpolate = FALSE)  +
+      
+      #Border  
+      tm_shape(pol) +
+      tm_polygons(alpha = 0.001, lwd=1.5) +
+      
+      #Border of counties 
+      tm_shape(wojewodztwa)+
+      tm_polygons(alpha = 0.01, lwd=0.7)+
+      
+      #Rivers    
+      tm_shape(rzeki)+
+      tm_lines(col="#2669d6", lwd=1.1) +
+      
+      #Lakes
+      tm_shape(jeziora)+
+      tm_polygons(col="#2669d6") +
+      
+      
+      #Title of the figure
+      tm_layout(
+        aes.palette = "div",
+        sepia.intensity = 0.2,
+        legend.just = "right",
+        title.color = "blue",
+        compass.type = "arrow",
+        title.bg.color = "white", 
+        title.bg.alpha = 0.5,
+        title.size = 45,
+        #title.position = c(0.02,0.06),
+        legend.outside = T,
+        legend.outside.position =  "bottom",
+        legend.width = 5,
+        legend.hist.width = 0.9,
+        legend.hist.height = 0.6,
+        legend.title.size = 0.90,
+        legend.text.size = 0.5,
+        #legend.position = c("right","bottom"),
+        legend.bg.color = "#FFFFFF60",
+        legend.height = 0.9,
+        legend.frame.lwd = 0.2,
+        legend.frame = F,
+        legend.bg.alpha = 1,
+        space.color="grey90"
+        #legend.format = list(text.separator = " ", format = formatC("f"))
+      )+
+      
+      #Lon/Lat    
+      tm_grid(projection = "longlat", x = 10:30, y=40:60, labels.col = "black", col = "gray",lwd = 0.5,
+              labels.size = 0.4, labels.inside.frame = T) + 
+      #Mean values of counties
+      tm_shape(centroidy)+
+      tm_text("column", size = 0.6) +   
+      
+      #Compass
+      tm_compass(size = 1, fontsize = 0.7,
+                 position = c(0.04,0.9), color.light = "grey90") +
+      
+      # scale bar
+      tm_scale_bar(width = 0.12,size = 0.35,breaks = c(0,50,100,150), position = c("left","bottom")) +
+      
+      # windhydro credits
+      tm_credits("esusza.pl\n(c) WIND-HYDRO 2020", position = c("left", "bottom"), 
+                 size = 0.35, bg.color = "white")
+    
+  }
+  
+
+    p = temperatura_map(input=liczba_godzin, title = paste0("Czas przymrozku w ciągu doby (h)\n", patt))
+    tmap_save(p,
+      filename = paste0("/home/wh/tsk/czas_przymrozku_dobowo_png/", 
+                        patt, 
+                        "_tsk_czas_przymrozku.png"), 
+      width=1000, height=1300)
+
+    
+# koniec, dymbiec, pentla!
--- R/tsk_daily.R
+++ R/tsk_daily.R
@@ -8,7 +8,7 @@ setwd("~/tsk/godzinowe/")
 files = dir(pattern = "_tsk.tif", full.names = F, recursive = T)
 files[1]
 
-df = seq.Date(from = as.Date("2019-12-31"), to = as.Date("2020-05-01"), by = "day")
+df = seq.Date(from = as.Date("1998-01-01"), to = as.Date("2020-05-01"), by = "day")
 
 srednia = function(x){
   r = stack(dir(pattern = as.character(x)))
@@ -24,11 +24,16 @@ pbmclapply(as.list(df), srednia, mc.cores = 31)
 
 ### teraz liczenie godzin z przekroczeniem progu:
 setwd("/home/wh/tsk/godzinowe/")
+df = seq.Date(from = as.Date("1998-01-01"), to = as.Date("2020-05-01"), by = "day")
 czas_przymrozku = function(x){
   r = stack(dir(pattern = as.character(x)))
   rc <- reclassify(r, c(-Inf,273.15,1, 273.15,+Inf,0))
   r = calc(x = rc, sum)
-  writeRaster(r, filename = paste0("/home/wh/tsk/czas_przymrozku_dobowo/",as.character(x),"_tsk_czas_przymrozku.tif"))
+  writeRaster(r, 
+              filename = paste0("/home/wh/tsk/czas_przymrozku_dobowo/",
+                                as.character(x),
+                                "_tsk_czas_przymrozku.tif"),
+              overwrite = T)
 }
 
 pbmclapply(as.list(df), czas_przymrozku, mc.cores = 31)
@@ -41,18 +46,51 @@ pbmclapply(as.list(df), czas_przymrozku, mc.cores = 31)
 # plan jest taki, zeby liczyc to na podstawie dobowek
 
 setwd("/home/wh/tsk/tmin_dobowe/")
-files = dir(pattern = "2020")
-r = stack(files)
-rc <- reclassify(r, c(-Inf,273.15,1, 273.15,+Inf,0))
 
-doy = rc # empty raster for doy
+dany_rok = function(rok){
+  files = dir(pattern = as.character(rok))
+  r = stack(files)
+  rc <- reclassify(r, c(-Inf,273.15,1, 273.15,+Inf,0))
+  
+  doy = rc # empty raster for doy
 
-for (i in 1:length(names(doy))){
-  print(i)
-  doy[[i]][] = i
-}
+  for (i in 1:length(names(doy))){
+    #print(i)
+    doy[[i]][] = i
+  }
 
-doy = rc * doy
+  doy = rc * doy
+  
+  
+  # ostatni mroz mozna nawet policzyc w petli jako:
+  
+  for (ind in 1:length(files)){
+    print(ind)
+    maksy = calc(doy[[1:ind]], max)
+    
+    # usuniecie zer glownie dla baltyku:
+    maksy[maksy==0] = NA
+    
+    # odejmujemy nr dzisiejszego dnia:
+    ostatni_mroz = ind-maksy
+    ostatni_mroz
+    writeRaster(ostatni_mroz, 
+                filename = paste0("/home/wh/tsk/ostatni_mroz/",
+                                  substr(files[ind],1,10),
+                                  "_tsk_ostatni_mroz.tif"),
+                overwrite=TRUE)
+    
+  }
+  
+  }
+
+
+pbmclapply(as.list(1998:2019), dany_rok, mc.cores = 31)
+
+
+pbmclapply(as.list(df), ostatni_mroz, mc.cores = 31)
+
+  
 
 ostatni_mroz = function(x){
   ind = grep(pattern = as.character(x), x = files)
@@ -60,16 +98,18 @@ ostatni_mroz = function(x){
   # plot(doy[[ind]])
   # max z wszystkich warstewek od 1 dnia do dzis:
   maksy = calc(doy[[1:ind]], max)
-  # usuniecie zer glownie dla baltyku:
-  maksy[maksy==0] = NA
   
-  # odejmujemy nr dzisiejszego dnia:
-  ostatni_mroz = ind-maksy
-  ostatni_mroz
-  writeRaster(ostatni_mroz, filename = paste0("/home/wh/tsk/ostatni_mroz/",as.character(x),"_tsk_ostatni_mroz.tif"),
-              overwrite=TRUE)
 }
 
   #sp::spplot(ostatni_mroz) # czyli ostatni dzien z przymrozkiem
 
 pbmclapply(as.list(df), ostatni_mroz, mc.cores = 31)
+
+
+
+# jak dorobic braki plikow?
+ # linki = paste0(seq.Date(from = as.Date("1998-01-01"), to = as.Date("2020-05-01"), by = "day"), "_tsk_tmin.tif")
+ # df = data.frame(path = linki, jestnadysku = file.exists(linki), stringsAsFactors = F)
+ # head(df)
+ # table(df$jestnadysku)
+# file.copy(from = df$path[which(!df$jestnadysku)-1], to = df$path[which(!df$jestnadysku)])
